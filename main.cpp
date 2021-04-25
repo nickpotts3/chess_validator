c36c3e081fbfd916f1bf69aa163a1e6b7da52863
Nicholas Potts <nicholaspotts@nicholass-imac.home>
2021-04-24 20:30:58 -0400

    class for each piece

--- main.cpp
+++ main.cpp
@@ -4,6 +4,69 @@
 //#include "board.h"
 using namespace std;
 
+//enum piece {i, j, k, l, m, n, o, p, q, r, s, t, u};
+enum color {White, Black};
+
+class Piece {
+public:
+	Piece() {};
+	Piece(const Piece& p);
+	~Piece() {}
+	string currentPos;
+	string color;
+	virtual bool move(string)=0;
+};
+
+
+Piece::Piece(const Piece& p) {
+	color = p.color;
+}
+
+class Rook : public Piece {
+public:
+	Rook() {};
+	bool move(string pos) {
+		cout << "Rook move called" << endl;
+		return true;
+	}
+
+};
+
+class Pawn : public Piece {
+	bool move(string pos) {
+		return true;
+	}
+};
+
+class Bishop : public Piece {
+	bool move(string pos) {
+		return true;
+	};
+};
+
+class Knight : public Piece {
+	bool move(string pos) {
+
+	}
+
+};
+
+class King : public Piece {
+	bool move(string pos) {
+	
+	}
+
+};
+
+class Queen : public Piece {
+	bool move(string pos) {
+
+	}
+};
+
+class Empty : public Piece {
+
+};
 
 class Board {
 
@@ -51,7 +114,32 @@ map<string, char> Board::getGameBoard() {
 	return game;
 }
 
+
+string determinePiece(char piece, string pos) {
+//	auto chessP;
+//	cout << "entered determinePiece at pos: " << pos << endl; 
+	if(piece == 'p' || piece == 'i') {
+		return "Rook";
+//		Rook r;
+//		return r;
+	} else if (piece == 'j' || piece == 'q') {
+		return "knight";
+	} else if (piece == 'k' || piece == 'r') {
+		return "Bishop";
+	} else if (piece == 'l' || piece == 's') {
+		return "Queen";
+	} else if (piece == 'm' || piece == 't') {
+		return "King";
+	} else if (piece == 'o' || piece == 'u') {
+		return "pawn";
+	} else {
+		return "piece";
+	}	
+}
+
 int main() {
+    Rook r;
+    cout << r.move("foo");   
     string line;
     map<string, char> boardGame;    
     Board boards[4];
@@ -76,7 +164,7 @@ int main() {
 	  } else {
 	    for(int i = 0; i < line.length(); i++) {
                 string pos = file[i] + to_string(rank);
-
+		cout << determinePiece(line[i], pos);
                 pair<string, char> location (pos, line[i]);
 		boardGame.insert(location);
 	    }
