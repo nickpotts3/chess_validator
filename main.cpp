0da8bc3d3d3345151aa88f17b1854deb857c9aa5
Nicholas Potts <nicholaspotts@Nicholass-iMac.local>
2021-04-26 21:14:05 -0400

    Current seg fault

--- main.cpp
+++ main.cpp
@@ -11,12 +11,21 @@ map<string, Piece*> boardGame;
 class Piece {
 public:
 	Piece() {};
+	Piece(string);
 	Piece(const Piece& p);
 	~Piece() {}
 	string currentPos;
 	string color;
+	friend ostream& operator<<(ostream& os, const Piece& p);
 	virtual string move(string)=0;
 };
+
+Piece::Piece(string pos) { currentPos = pos; }
+
+ostream& operator<<(ostream& os, const Piece& p) {
+	os << p.color << " : " << p.currentPos << endl;
+	return os;
+}
 
 
 Piece::Piece(const Piece& p) {
@@ -25,61 +34,75 @@ Piece::Piece(const Piece& p) {
 
 class Rook : public Piece {
 public:
+	Rook(string){};
 	Rook() {cout << "Rook ctr called" << endl;};
 	string move(string pos) {
-		return "Rook move called";
+		//board validation is done before this is executed
+			return "MOVE ROOK TO" + pos;
 	}
+	void validMove(string);
 
 };
 
 class Pawn : public Piece {
 public:
+	Pawn(string){};
 	Pawn() {cout << "Pawn ctr called" << endl;};
 	string move(string pos) {
-		return "pawn move";
+		return "MOVE PAWN TO " + pos;
 	}
+	void validMove(string);
 };
 
 class Bishop : public Piece {
 public:
+	Bishop(string){};
 	Bishop() {cout << "Bishop ctr called" << endl;};
 	string move(string pos) {
-		return "bishop move";
-	};
+		return "bishop move to " + pos ;
+	}
+	void validMove(string);
 };
 
 class Knight : public Piece {
 public:
+	Knight(string){};
 	Knight() {cout << "Knight ctr called" << endl;};
 	string move(string pos) {
-		return "knight";
+		return "knight move to " + pos;
 	}
-
+	void validMove(string);
 };
 
 class King : public Piece {
 public:
+	King(string){};
 	King() {cout << "king ctr called" << endl;};
 	string move(string pos) {
-		return "King";
+		return "King move to " + pos;
 	}
+	void validMove(string);
 
 };
 
 class Queen : public Piece {
 public:
+	Queen(string){};
 	Queen() {cout << "Queen ctr called" << endl;};
 	string move(string pos) {
 		return "queen";
 	}
+	void validMove(string);
 };
 
 class Empty : public Piece {
 public:
+	Empty(string){};
 	Empty(){cout << "empty ctr called" << endl;};
 	string move(string pos) {
 		return "Empty";
 	}
+	void validMove(string);
 };
 
 class Board {
@@ -129,54 +152,64 @@ map<string, Piece*> Board::getGameBoard() {
 }
 
 
-pair<string,Piece*> makePair(Piece *piece, string pos) {
-	return pair<string, Piece*> (pos, piece);
+pair<string,Piece*> makePair(Piece &piece, string pos) {
+//	cout << "INSIDE MAKE PAIR" << " pos is " << pos << endl; // " " << piece.move("pos") <<  endl;
+	return (pair<string, Piece*> (pos, &piece));
 }
 void determinePiece(char piece, string pos) {
-//	auto chessP;
-	cout << "entered determinePiece at pos: " << pos << endl; 
+//	cout << "PIECE IS : " << piece << " AT POS: " << pos << endl;
+//	cout << "entered determinePiece at pos: " << pos << endl; 
 	if(piece == 'p' || piece == 'i') {
-		Rook r;
-		boardGame.insert (makePair(&r , pos));
+		Rook* r = new Rook(pos);
+//		cout << r.move(pos) << endl;
+		boardGame.insert(makePair(*r, pos));
+		cout << "AFTER INSERT ROOK" << endl;
 	} else if (piece == 'j' || piece == 'q') {
-		Knight k;
-		boardGame.insert( makePair(&k, pos));
-//		return "knight";
+		Knight k(pos);
+//		cout << k.move(pos) << endl;
+		boardGame.insert(makePair(k, pos));
 	} else if (piece == 'k' || piece == 'r') {
-		Bishop b;
-		boardGame.insert(makePair(&b, pos));
-//		return "Bishop";
+		Bishop b(pos);
+//		cout << b.move(pos) << endl;
+		boardGame.insert(makePair(b, pos));
 	} else if (piece == 'l' || piece == 's') {
-		Queen q;
-		boardGame.insert(makePair(&q, pos));
-//		return "Queen";
+		Queen q(pos);
+//		cout << q.move(pos) << endl;
+		boardGame.insert(makePair(q, pos));
 	} else if (piece == 'm' || piece == 't') {
-		King kg;
-		boardGame.insert(makePair(&kg, pos));
-//		return "King";
+		King kg(pos);
+//		cout << kg.move(pos) << endl;
+		boardGame.insert(makePair(kg, pos));
 	} else if (piece == 'o' || piece == 'u') {
-//		cout << "Found pawn" << endl;
-		Pawn p;
-		boardGame.insert(makePair(&p, pos));
-//		return "pawn";
+		Pawn p(pos);
+//		cout << p.move(pos) << endl;
+		boardGame.insert(makePair(p, pos));
 	} else {
-		Empty e;
-//		boardGame.insert(makePair(&e, pos));
-//		return "piece";
-	}	
+		Empty e(pos);
+//		cout << e.move(pos) << endl;
+		boardGame.insert(makePair(e,pos));
+	}
+//	cout << "---leaving determinePiece---" << endl;	
 }
 
-//map<string, char> boardGame;
-int main() {
-//    Rook r;
-  //  cout << r.move("foo");   
+void validateMoveSet(string moveSet) {
+	cout << "Move Set called: " << moveSet << endl;
+	string move = moveSet.substr(0, 2);
+	cout << move << endl;
+	auto chessP = boardGame.find(move)->second;
+	cout << (*chessP).move("Foo") << endl;
+//	cout << boardGame.find(move)->second*->move(move); // << "   :MOVE" << endl;
+//	cout << (*chessP).move(move);
+}
+
+
+int main() {   
     string line;
-   // map<string, char> boardGame;    
+    string moveSet;       
     Board boards[4];
     int count = 0;
     int rank = 8; // to store the row name in the key for the map
-    char file[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}; // to store the column name in the key for the boardGame map
-//    map<string, char>::iterator it = boardGame.begin();
+    char file[8] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}; // to store the column name in the key for the boardGame map
     ifstream boardFile;
     ifstream moveFile ("move_sets.txt");
     boardFile.open("board_states.txt");
@@ -186,22 +219,23 @@ int main() {
 	  if(line.length() == 0) {
 	    Board tmp(boardGame);
 	    boards[count] = tmp;
+	    getline(moveFile, moveSet);
+	    validateMoveSet(moveSet);
+            string move = moveSet.substr(0, 4);
+	    cout << move << endl;
+	    cout << moveSet << endl;
 	    ++count;
-//	    cout << count << endl;
-  //          cout << "---------------------------- NEW BOARD ------------------------" << endl;
             rank = 8;
             boardGame.clear(); 
 	  } else {
-	    for(int i = 0; i < line.length(); i++) {
+	    for(int i = 0; i < line.length() - 1; i++) {
                 string pos = file[i] + to_string(rank);
 		determinePiece(line[i], pos);
-                pair<string, char> location (pos, line[i]);
-//		boardGame.insert(location);
 	    }
 	    --rank;
-	    if(rank < 0) {rank = 8;}
+	    if(rank == 0) {rank = 8;}
 	    for(auto it = boardGame.cbegin(); it != boardGame.cend(); ++it) {
-		cout << (*it).first << " " << (*(*it).second).move("foo");
+	//	cout << (*it).first << endl; // " " << (*(*it).second).move("foo");
 	    }
 	  }
 	} // end while loop
@@ -210,21 +244,16 @@ int main() {
     } else {
 	cout << "Closed";
     }
-    cout << "See me" << endl;
-    //moveFile.open("move_sets.txt");
-//    for( auto it = boardGame.cbegin(); it != boardGame.cend(); ++it) {
-//	cout << (*it).first << " : " << (*it).second.move() << endl;
-  //  }
     string moves;
-    if(moveFile.is_open()) {
-	cout << "Open" << endl;
-	while(getline(moveFile, moves)) {
-	  cout << moves;
-	  cout << endl;
-	}
-    } else {
-	cout << "Closed";
-    }
+   // if(moveFile.is_open()) {
+//	cout << "Open" << endl;
+//	while(getline(moveFile, moves)) {
+//	  cout << moves;
+//	  cout << endl;
+//	}
+  //  } else {
+//	cout << "Closed";
+  //  }
 
 	    
     return(0);
